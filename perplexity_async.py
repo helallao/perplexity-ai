ccbfd4b954fe8a36660ec2b414e97739c66f139f
helallao <78656003+helallao@users.noreply.github.com>
2024-01-30 17:03:40 +0300

    Update perplexity_async.py

--- perplexity_async.py
+++ perplexity_async.py
@@ -491,7 +491,7 @@ class LabsClient(AsyncMixin):
 
     # method to ask to perplexity labs
     async def ask(self, query, model='pplx-7b-online'):
-        assert model in ['pplx-7b-online', 'pplx-70b-online', 'pplx-7b-chat', 'pplx-70b-chat', 'mistral-7b-instruct', 'codellama-34b-instruct', 'llama-2-70b-chat', 'llava-7b-chat', 'mixtral-8x7b-instruct', 'mistral-medium', 'related'], 'Search modes --> ["pplx-7b-online", "pplx-70b-online", "pplx-7b-chat", "pplx-70b-chat", "mistral-7b-instruct", "codellama-34b-instruct", "llama-2-70b-chat", "llava-7b-chat", "mixtral-8x7b-instruct", "mistral-medium", "related"]'
+        assert model in ['pplx-7b-online', 'pplx-70b-online', 'pplx-7b-chat', 'pplx-70b-chat', 'mistral-7b-instruct', 'codellama-34b-instruct', 'codellama-70b-instruct', 'llama-2-70b-chat', 'llava-7b-chat', 'mixtral-8x7b-instruct', 'mistral-medium', 'related'], 'Search modes --> ["pplx-7b-online", "pplx-70b-online", "pplx-7b-chat", "pplx-70b-chat", "mistral-7b-instruct", "codellama-34b-instruct", "codellama-70b-instruct", "llama-2-70b-chat", "llava-7b-chat", "mixtral-8x7b-instruct", "mistral-medium", "related"]'
 
         self._last_answer = None
         self.history.append({'role': 'user', 'content': query, 'priority': 0})
@@ -508,6 +508,7 @@ class LabsClient(AsyncMixin):
                     'pplx-70b-chat': 'pplx-70b-chat',
                     'mistral-7b-instruct': 'mistral-7b-instruct',
                     'codellama-34b-instruct': 'codellama-34b-instruct',
+                    'codellama-70b-instruct': 'codellama-70b-instruct',
                     'llama-2-70b-chat': 'llama-2-70b-chat',
                     'llava-7b-chat': 'llava-v1.5-7b-wrapper',
                     'mixtral-8x7b-instruct': 'mixtral-8x7b-instruct',
