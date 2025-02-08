e64308f34d93dc3abdbf020fc23f0d67884c0a94
Ali Yaşar <78656003+helallao@users.noreply.github.com>
2025-02-08 20:38:14 +0300

    Update __init__.py

--- perplexity/__init__.py
+++ perplexity/__init__.py
@@ -1,6 +1,5 @@
 from perplexity.client import Client
 from perplexity.emailnator import Emailnator
 from perplexity.labs import LabsClient
-from perplexity.pool import Pool
 
-__all__ = ['Client', 'Emailnator', 'LabsClient', 'Pool']
+__all__ = ['Client', 'Emailnator', 'LabsClient']
