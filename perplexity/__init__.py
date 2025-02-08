9dd6f8905c8dce1818eb6f30768008e7fb58ab4b
Ali Yaşar <78656003+helallao@users.noreply.github.com>
2025-02-08 21:18:45 +0300

    Add files via upload

--- perplexity/__init__.py
+++ perplexity/__init__.py
@@ -1,5 +1,5 @@
-from perplexity.client import Client
-from perplexity.emailnator import Emailnator
-from perplexity.labs import LabsClient
+from .client import Client
+from .emailnator import Emailnator
+from .labs import LabsClient
 
 __all__ = ['Client', 'Emailnator', 'LabsClient']
